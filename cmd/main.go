fb8bf5f0859a6c4f5463b0f4810154f2ca814355
binh.buidang <dangbinh4869@gmail.com>
2022-09-19 14:00:07 +0700

    chore: add gst.

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,80 @@
 package main
 
 import (
-	"github.com/dunkbing/meeting-bot/bot"
-	"github.com/dunkbing/meeting-bot/config"
+	"fmt"
+	"github.com/tinyzimmer/go-gst/gst"
+	"os"
 )
 
 func main() {
-	_, _ = config.Get()
-	b := bot.New()
-	if b == nil {
-		return
+	//_, _ = config.Get()
+	fmt.Println("start")
+	//b := bot.New()
+	//if b == nil {
+	//	return
+	//}
+	//if b.Type == bot.GoogleMeet {
+	//	b.JoinGoogleMeet()
+	//}
+
+	// This example expects a simple `gst-launch-1.0` string as arguments
+	if len(os.Args) == 1 {
+		fmt.Println("Pipeline string cannot be empty")
+		os.Exit(1)
 	}
-	if b.Type == bot.GoogleMeet {
-		b.JoinGoogleMeet()
-	}
+
+	// Initialize GStreamer with the arguments passed to the program. Gstreamer
+	// and the bindings will automatically pop off any handled arguments leaving
+	// nothing but a pipeline string (unless other invalid args are present).
+	gst.Init(&os.Args)
+
+	// Create a main loop. This is only required when utilizing signals via the bindings.
+	// In this example, the AddWatch on the pipeline bus requires iterating on the main loop.
+	//mainLoop := glib.NewMainLoop(glib.MainContextDefault(), false)
+	//
+	//// Build a pipeline string from the cli arguments
+	//pipelineString := strings.Join(os.Args[1:], " ")
+	//
+	///// Let GStreamer create a pipeline from the parsed launch syntax on the cli.
+	//pipeline, err := gst.NewPipelineFromString(pipelineString)
+	//if err != nil {
+	//    fmt.Println(err)
+	//    os.Exit(2)
+	//}
+	//
+	//// Add a message handler to the pipeline bus, printing interesting information to the console.
+	//pipeline.GetPipelineBus().AddWatch(func(msg *gst.Message) bool {
+	//    switch msg.Type() {
+	//    case gst.MessageEOS: // When end-of-stream is received flush the pipeling and stop the main loop
+	//        err := pipeline.BlockSetState(gst.StateNull)
+	//        if err != nil {
+	//            fmt.Println(err.Error())
+	//            return false
+	//        }
+	//        mainLoop.Quit()
+	//    case gst.MessageError: // Error messages are always fatal
+	//        err := msg.ParseError()
+	//        fmt.Println("ERROR:", err.Error())
+	//        if debug := err.DebugString(); debug != "" {
+	//            fmt.Println("DEBUG:", debug)
+	//        }
+	//        mainLoop.Quit()
+	//    default:
+	//        // All messages implement a Stringer. However, this is
+	//        // typically an expensive thing to do and should be avoided.
+	//        fmt.Println(msg)
+	//    }
+	//    return true
+	//})
+	//
+	//// Start the pipeline
+	//fmt.Println("start the pipeline")
+	//err = pipeline.SetState(gst.StatePlaying)
+	//if err != nil {
+	//    fmt.Println(err.Error())
+	//    return
+	//}
+	//
+	//// Block and iterate on the main loop
+	//mainLoop.Run()
 }
--- go.mod
+++ go.mod
@@ -5,9 +5,12 @@ go 1.19
 require (
 	github.com/go-rod/rod v0.109.3
 	github.com/joho/godotenv v1.4.0
+	github.com/tinyzimmer/go-glib v0.0.25
+	github.com/tinyzimmer/go-gst v0.2.33
 )
 
 require (
+	github.com/mattn/go-pointer v0.0.1 // indirect
 	github.com/ysmood/goob v0.4.0 // indirect
 	github.com/ysmood/gson v0.7.1 // indirect
 	github.com/ysmood/leakless v0.8.0 // indirect
